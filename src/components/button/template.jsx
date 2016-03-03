9cb82ccc608f3a557b63bf343950010617e7ed4e
Jonathan Svenheden <jonathan@svenheden.com>
2016-03-03 11:27:02 +0100

    Add component documentation example

--- src/components/button/template.jsx
+++ src/components/button/template.jsx
@@ -1,5 +1,12 @@
 var React = require('react');
 
+/**
+ * This component can be used to provide the user a simple way to trigger an event.
+ *
+ * Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean congue tincidunt dolor, sed
+ * vehicula lectus cursus eu. In commodo, justo sit amet elementum imperdiet, mi felis vehicula
+ * ipsum, tempus pretium sapien mauris sed nunc. Interdum et malesuada fames ac ante ipsum primis.
+ */
 var Button = function(props) {
   var className = 'button button--' + props.color;
 
@@ -12,6 +19,10 @@ Button.displayName = 'Button';
 
 Button.propTypes = {
   color: React.PropTypes.oneOf(['red', 'blue']),
+
+  /**
+   * The text that should appear in the button
+   */
   label: React.PropTypes.string.isRequired
 };
 
