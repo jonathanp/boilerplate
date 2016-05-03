70b1ef26b840ee658088c2f93f201ac5b777e95d
Jonathan Svenheden <jonathan@svenheden.com>
2016-05-03 14:07:20 +0200

    Add husky and `prepush` hook
    https://github.com/typicode/husky

--- src/components/button/client.js
+++ src/components/button/client.js
@@ -2,6 +2,6 @@ var button = document.querySelector('.button');
 
 if (button) {
   button.addEventListener('click', function() {
-    console.log('clicked button');
+    // console.log('clicked button');
   });
 }
